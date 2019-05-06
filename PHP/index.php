edd60339ae8a3580eeedef9b9188addfecabb6ba
Dani <d.kools1@outlook.com>
2019-05-06 10:19:50 +0200

    Teams show up in the index

--- PHP/index.php
+++ PHP/index.php
@@ -1,5 +1,10 @@
 <?php
-require 'header.php'
+require 'header.php';
+
+$sql = "SELECT * FROM teams";
+$query = $db->query($sql);
+$teams = $query->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -22,6 +27,19 @@ require 'header.php'
                 echo "<p>You are logged in, do you want to <a href='logout.php'>log out?</a></p>";
 
                 echo "<p>You can create a new team <a href='createteam.php'>here!</a></p>";
-            }?>
+            ?>
+
+                <h1>Teams</h1>
+                <ul>
+                    <?php
+                    foreach ($teams as $team) {
+                        echo "<li>${team['teamname']}</li>";
+                    }
+                    ?>
+                </ul>
+
+            <?php
+            }
+            ?>
 </body>
 </html>
