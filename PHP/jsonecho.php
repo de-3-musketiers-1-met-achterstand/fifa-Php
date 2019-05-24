3bb622bfefcf27f0f0600f01408f3dd9b2b47a79
Dani <d.kools1@outlook.com>
2019-05-24 08:59:41 +0200

    Deleted echo test

--- PHP/jsonecho.php
+++ PHP/jsonecho.php
@@ -18,7 +18,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
                     $sql = "SELECT * FROM teams";
                     $query = $db->query($sql);
                     $teams = $query->fetchAll(PDO::FETCH_ASSOC);
-                    echo 'test';
+
 
 
                     echo json_encode($teams);
